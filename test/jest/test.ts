e224fc00ddd6531e481483c9a41ddad4c3967e65
Fernanda Andrade <flandradeg@gmail.com>
2018-01-01 22:40:56 -0500

    Add test suite for jest

--- test/jest/test.ts
+++ test/jest/test.ts
@@ -1,23 +1,23 @@
 "use strict";
 
 import * as Promise from "bluebird";
+import "jest";
 
 import scenario from "../../src/scenario-01";
-import { expect } from "./init";
 
-describe("Scenario", function() {
+test("Scenario", function() {
   return runScenario(200);
 });
 
 function runScenario(times: number): void {
   const values = [...Array(times).keys()];
   values.map(value => {
-    describe(`test with mocha #${value}`, function() {
-      context("when all promises are resolved", function() {
+    describe(`test with jest #${value}`, function() {
+      describe("when all promises are resolved", function() {
         it("returns the values", function() {
           return expect(
             scenario()
-          ).to.not.be.rejected;
+          ).resolves.toBe(1);
         });
       });
     });
